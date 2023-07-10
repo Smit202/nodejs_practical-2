761091dc947f30a823758ad67bd86634afc2a176
Smit202 <smit.sheth@simformsolutions.com>
2023-07-10 17:50:08 +0530

    nodejs practical-2

--- app.js
+++ app.js
@@ -22,16 +22,9 @@ app.get('/', (req, res, next) => {
 function checkStatus(schedule) {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     let date = new Date();
-    // console.log(date);
-    // console.log(date.getTimezoneOffset());
-    // date = new Date(date.getTime() - date.getTimezoneOffset()*60*1000);
-    console.log(date); 
+    console.log(date);
     // date.setHours(19);
-    // date.setMinutes(49);
-    // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone); 
-    currentDay = date.getDay();
-    currentHours = date.getHours();
-    currentMinutes = date.getMinutes();
+    // date.setMinutes(0);
     let currentTime = [date.getHours(), date.getMinutes(), date.getDay()];
     const daySchedule = schedule.find(item => item.day === days[currentTime[2]]);
 
@@ -49,22 +42,29 @@ function checkStatus(schedule) {
 
         // Converting 12 hour open and close time format to 24 hour format
         if (openTime[2] === 'PM' && openTime[0] !== 12) openTime[0] += 12;
-        else if (openTime[2] === 'AM' && openTime[0] === 12) openTime[0] = 0;
+        if (openTime[2] === 'AM' && openTime[0] === 12) openTime[0] = 0;
         if (closeTime[2] === 'PM' && closeTime[0] !== 12) closeTime[0] += 12;
-        else if (closeTime[2] === 'AM' && closeTime[0] === 12) openTime[0] = 0;
+        if (closeTime[2] === 'AM' && closeTime[0] === 12) openTime[0] = 0;
 
         console.log(openTime);
         console.log(closeTime);
 
-        if (currentTime[0] < openTime[0])   return 'Closed';
-        if (currentTime[0] === openTime[0]) {
-            return currentTime[1] < openTime[1] ? 'Closed' : 'Open';
-        }
-        if (currentTime[0] === closeTime[0]) {
-            return currentTime[1] > closeTime[1] ? 'Closed' : 'Open';
-        }
-        if(currentTime[0] > openTime[0] && currentTime[0] < closeTime[0])   return 'Open';
-        return 'Closed';
+        const openDate = new Date(date.getTime());
+        openDate.setHours(openTime[0]);
+        openDate.setMinutes(openTime[1]);
+
+        const closeDate = new Date(date.getTime());
+        closeDate.setHours(closeTime[0]);
+        closeDate.setMinutes(closeTime[1]);
+
+        const currentTimestamp = date.getTime();
+        const openTimestamp = openDate.getTime();
+        const closeTimestamp = closeDate.getTime();
+
+        console.log(currentTimestamp, openTimestamp, closeTimestamp);
+
+        if(currentTimestamp < openTimestamp || currentTimestamp > closeTimestamp) return 'Closed';
+        return 'Open';
     }
     
     return 'Closed';
